67c87fb37b02d6d3ec39b381efc6e8ac1789344a
mansuri-sabit <blueflamesabitali2003@gmail.com>
2025-07-10 10:21:44 +0530

    change auth

--- src/services/auth.js
+++ src/services/auth.js
@@ -2,7 +2,7 @@ import api from './api';
 
 export const authService = {
   login: async (email, password) => {
-    const response = await api.post('/auth/login', { email, password });
+    const response = await api.post('api/auth/login', { email, password });
     return response.data;
   },
 
